9f50746c970e0a6d9dfd43a88c8c340f50dfa950
ryudi1234 <ryudikitano@gmail,com>
2024-09-09 21:00:25 -0300

    commit 6

---
+++ dados.js
@@ -0,0 +1,23 @@
+let dados = [
+    {
+        titulo : "Ítalo Ferreira",
+        descricao : "surfista Ítalo Ferreira, nascido em Baía Formosa, Rio Grande do Norte, é um dos maiores nomes do surfe mundial. Com sua habilidade única e carisma contagiante, conquistou o coração dos brasileiros e se tornou um verdadeiro ícone do esporte.",
+        link : "https://ge.globo.com/surfe/wsl/noticia/2024/08/23/veja-os-surfistas-que-disputarao-o-titulo-mundial-de-2024-no-wsl-finals.ghtml",
+        tags : "surf surfista"
+    },
+    {
+        titulo: "Rebeca Andrade: A Fada do Esporte",
+        descricao: "Jinasta Rebeca Andrade, natural de Olímpia, São Paulo, é uma ginasta que encantou o mundo com sua habilidade e carisma. Conquistou diversas medalhas em competições internacionais e se tornou um ícone do esporte brasileiro.",
+        link: "https://www.cbg.org.br/atletas/rebeca-andrade/",
+        tags : "ginástica ginastica ouro"
+    },
+    {
+        titulo: "Rafaela Silva: A Leoa do Judô",
+        descricao: "Judoca Rafaela Silva, nascida no Rio de Janeiro, é uma das maiores judocas da história do Brasil. Com sua força e determinação, conquistou o ouro olímpico e se tornou um símbolo de superação.",
+        link: "https://www.cob.org.br/noticias/rafaela-silva/",
+        tags : "judô judo judoca judoka"
+    }
+
+];
+
+
